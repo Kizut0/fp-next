7aa308aa9573e32b49d3a5c9135ad6d269ea7c95
aungmyatoogyaw <aungmyatoogyaw@gmail.com>
2026-02-23 20:27:41 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/auth/:path*",
+        destination: "/api/auth/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
